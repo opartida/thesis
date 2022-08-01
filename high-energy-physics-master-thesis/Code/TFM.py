448394f0ba8b5ebafe6bcd40a00c88c1d9aec972
Oliver Partida <oliver.partida@sage.com>
2022-08-02 01:00:55 +0200

    add high energy physics master thesis

---
+++ high-energy-physics-master-thesis/Code/TFM.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal
+"""
+from __future__ import division
+from sympy import *
+x, y, z, t = symbols('x y z t')
+print(integrate(x**2 + x + 1, x))
